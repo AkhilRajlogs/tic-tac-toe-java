9be6bd8230104fa3615fd4c08292f2de1fd677d4
Akhil Raj <akhilrajwork@gmail.com>
2026-02-11 23:31:05 +0530

    Add interactive turn-based gameplay loop

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -21,18 +21,39 @@ public class TicTacToe {
         player2 = takePlayerInput(2, s);
         
         //check duplicate symbol
-        while(player1.getSymbol()==player2.getSymbol()){
+        while (player1.getSymbol()==player2.getSymbol()){
             System.out.println("Symbol already taken!! Please re-enter a different symbol.");
             player2.setSymbol(s.next().charAt(0));
             s.nextLine(); // to clean up left over and \n before next scan
         }
 
-        //Create the Board
-            
+        //Create the Board            
             board = new Board(player1.getSymbol(), player2.getSymbol());
 
             board.printBoard();
         //Play the Game
+        boolean player1Turn = true;
+
+        //to be upgraded later
+        while (true) {
+            Player currentPlayer = player1Turn ? player1 : player2;
+            System.out.print("Hi "+ currentPlayer.getName() + ", Enter row (1-3) of your move: ");
+            int row = s.nextInt() - 1;
+
+            System.out.print("Hi "+ currentPlayer.getName() + ", Enter column (1-3) of your move: ");
+            int col = s.nextInt() - 1;
+            s.nextLine();
+
+            boolean moveMade = board.makeMove(row, col, currentPlayer.getSymbol());
+
+            if (!moveMade) {
+                System.out.println("Invalid move! Try again.");
+                continue;
+            }
+
+            board.printBoard();
+            player1Turn = !player1Turn;
+        }
     }
 
     private Player takePlayerInput(int num, Scanner s){
